4c045c25851271bee31e5fdade4a466789b87882
柒子小姐 <itheima@itcast.cn>
2024-06-17 16:36:47 +0800

    home: 静态组件的搭建和组件的深度选择样式修改

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,17 @@ import Hospital_bottom from '@/components/hospital_bottom/index.vue'
 //引入vue-router核心插件并安装
 import router from '@/router/index.ts'
 
+
+//引入elment-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 //利用createApp方法创建应用实例，且将应用实例挂载到，静态index.html挂载点
 const app = createApp(App)
 app.component('Hospital_top', Hospital_top)
 app.component('Hospital_bottom', Hospital_bottom)
 //安装路由插件
 app.use(router)
+//安装element-plus
+app.use(ElementPlus)
 app.mount("#app")
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    这是home首页
+    <!-- 轮播图首页结构 -->
+    <Carousel />
+    <!-- 首页搜索表单区域 -->
+    <Search />
   </div>
 </template>
 
 <script setup lang="ts">
-
+// 引入轮播图组件
+import Carousel from './carousel/index.vue'
+// 引入首页搜索组件
+import Search from './search/index.vue'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
