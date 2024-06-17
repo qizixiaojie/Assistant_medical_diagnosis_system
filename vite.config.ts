62bdf6189398848ea86d1f3e45c9b1e0f11980ba
柒子小姐 <itheima@itcast.cn>
2024-06-17 11:03:47 +0800

    项目的基本配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+//因为有时候写的地址相对很杂，所以进行地址的简写，src的绝对路径
+//导入node.js里面包路径来写
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //src文件的配置名
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,'src'),
+    }
+  }
 })
