2cc6566b5451bb1a88f88c415cbf6146726a3e40
柒子小姐 <itheima@itcast.cn>
2024-06-25 11:54:21 +0800

    hospital：预约挂号静态页面的搭建，医生部分

--- src/pages/hospital/register/register_setpOne.vue
+++ src/pages/hospital/register/register_setpOne.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="wrap">
+    <!-- 顶部结构 -->
     <div class="top">
       <div class="hosname">{{ workData.baseMap?.hosname }}</div>
       <div class="line"></div>
       <div>专科</div>
       <div class="hosdeparment">{{ workData.baseMap?.depname }}</div>
     </div>
+
+    <!-- 中间日期的结构 -->
     <div class="center">
       <h1>{{ workData.baseMap?.workDateString }}</h1>
       <div class="container">
@@ -24,6 +27,64 @@
         </div>
       </div>
       <el-pagination v-model:current-page="pageNo" layout="prev,pager,next" :total="workData.total" @current-change="fetchWorkData"></el-pagination>
+    </div>
+
+    <!-- 展示医生的结构 -->
+    <div class="bottom">
+      <!-- 展示即将放号的时间 -->
+      <div class="willing" v-show="false">
+        <p>2023.06.08</p>
+        <span>放号</span>
+      </div>
+      <!-- 展示医生上下午的结构  -->
+      <div class="doctor">
+        <div class="moring">
+          <!-- 上部分的文字提示 -->
+          <div class="tips">
+            <img src="@/assets/images/hospital_上午.png" style="width: 24px" />
+            <span>上午</span>
+          </div>
+          <!-- 展示每一个医生的信息 -->
+          <div class="doctor_Info">
+            <!-- 医生名字 -->
+            <div class="left">
+              <div class="info">
+                <span>副主任医生</span>
+                <p class="line"></p>
+                <span>柒子小姐</span>
+              </div>
+              <div class="skill">骨质疏松和骨代谢疾病、糖尿病、甲状腺疾病。</div>
+            </div>
+            <!-- 右侧挂号钱数 -->
+            <div class="right">
+              <div class="money">￥100</div>
+              <el-button type="primary" size="50" style="width: 140px; height: 40px; font-weight: 700; font-size: 16px">100</el-button>
+            </div>
+          </div>
+        </div>
+        <div class="after">
+          <div class="tips">
+            <img src="@/assets/images/hospital_下午.png" style="width: 24px" />
+            <span>下午</span>
+          </div>
+          <div class="doctor_Info">
+            <!-- 医生名字 -->
+            <div class="left">
+              <div class="info">
+                <span>副主任医生</span>
+                <p class="line"></p>
+                <span>柒子小姐</span>
+              </div>
+              <div class="skill">骨质疏松和骨代谢疾病、糖尿病、甲状腺疾病。</div>
+            </div>
+            <!-- 右侧挂号钱数 -->
+            <div class="right">
+              <div class="money">￥100</div>
+              <el-button type="primary" size="50" style="width: 140px; height: 40px; font-weight: 700; font-size: 16px">100</el-button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -77,7 +138,7 @@ const fetchWorkData = async () => {
     }
   }
   .center {
-    margin: 150px 0px 20px 0px;
+    margin: 100px 0px 20px 0px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -94,6 +155,7 @@ const fetchWorkData = async () => {
         margin: 0px 5px;
         text-align: center;
         border-radius: 5px;
+        cursor: pointer;
         &.active {
           border: 1px solid #b1a9a9;
           background-color: #eee;
@@ -160,6 +222,113 @@ const fetchWorkData = async () => {
         //   color: #b1a9a9;
         //   font-weight: 700;
         // }
+      }
+    }
+  }
+  .bottom {
+    font-size: 14px;
+    .willing {
+      display: flex;
+      justify-content: center;
+      font-size: 28px;
+      font-weight: 700;
+      p {
+        color: #1296db;
+      }
+    }
+    .doctor {
+      .moring {
+        .tips {
+          display: flex;
+          align-items: center;
+          span {
+            color: #b1a9a9;
+            margin: 0px 10px;
+            font-size: 14px;
+          }
+        }
+        .doctor_Info {
+          display: flex;
+          justify-content: space-between;
+          margin: 5px 0px;
+          border-bottom: 1px solid #b1a9a9;
+          .left {
+            .info {
+              display: flex;
+              align-items: center;
+              margin: 20px 0px;
+              font-size: 18px;
+              color: #1296db;
+              .line {
+                width: 1.5px;
+                height: 14px;
+                margin: 0px 10px;
+                background-color: #1296db;
+              }
+            }
+            .skill {
+              color: #b1a9a9;
+              margin-bottom: 10px;
+            }
+          }
+          .right {
+            display: flex;
+            align-items: center;
+            .money {
+              color: #1296db;
+              font-weight: 700;
+              font-size: 16px;
+              margin-right: 20px;
+            }
+          }
+        }
+      }
+      .after {
+        margin-top: 50px;
+        .tips {
+          display: flex;
+          align-items: center;
+          span {
+            color: #b1a9a9;
+            margin: 0px 10px;
+            font-size: 14px;
+          }
+        }
+        .doctor_Info {
+          display: flex;
+          justify-content: space-between;
+          margin: 5px 0px;
+          border-bottom: 1px solid #b1a9a9;
+          .left {
+            .info {
+              display: flex;
+              align-items: center;
+              margin: 20px 0px;
+              font-size: 18px;
+              color: #1296db;
+              .line {
+                width: 1.5px;
+                height: 14px;
+                margin: 0px 10px;
+                background-color: #1296db;
+              }
+            }
+            .skill {
+              color: #b1a9a9;
+              margin-bottom: 10px;
+            }
+          }
+          .right {
+            display: flex;
+            align-items: center;
+            .money {
+              color: #1296db;
+              font-weight: 700;
+              font-size: 16px;
+              margin-right: 20px;
+            }
+          }
+        }
       }
     }
   }
