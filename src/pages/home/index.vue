5f61f6e04a08330e73d856153a88c6f7788e5947
柒子小姐 <itheima@itcast.cn>
2024-06-28 10:20:02 +0800

    home：user中的search搜索的基本使用

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>这是home首页</div>
+  <!-- 轮播图组件 -->
+  <carousel />
+  <!-- 搜索组件 -->
+  <search />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//导入轮播图组件
+import carousel from '@/pages/home/carousel/index.vue'
+//引入首页搜索的组件
+import search from '@/pages/home/search/index.vue'
+</script>
 
 <style scoped></style>
---
+++ src/pages/home/search/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <!-- 像是使用组件都是要深度选择的 -->
+    <el-autocomplete :trigger-on-focus="false" placeholder="请你输入医院名称" style="width: 600px" />
+    <el-button type="primary" szie="default" :icon="Search" style="height: 48px">搜索</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+//引入element-plus提供的图标
+import { Search } from '@element-plus/icons-vue'
+</script>
+<script lang="ts">
+export default {
+  name: 'search'
+}
+</script>
+
+<style scoped lang="scss">
+.search {
+  display: flex;
+  width: 100%;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0px;
+  :deep(.el-input__wrapper) {
+    height: 48px;
+    margin-right: 10px;
+  }
+}
+</style>
